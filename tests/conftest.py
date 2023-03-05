7f176f38fb483c1e48ba617e44877970f4dd849d
Vladislav <djefry.kamen@gmail.com>
2023-03-05 22:55:07 +0400

    add selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,44 @@
+import os
+
 import pytest
-from selene import browser
+from selenium import webdriver
+from selene.support.shared import browser
+from selenium.webdriver.chrome.options import Options
+from huntflow.utils import attach
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def setup_chrome():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
     browser.config.base_url = 'https://huntflow.ru'
 
+    yield browser
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
